7a052124db255926dd78093708aec30ef55cf234
hanifb1360 <hanif.bahari@gmail.com>
2022-08-31 22:30:02 +0200

    some css

--- src/components/ReactPost.jsx
+++ src/components/ReactPost.jsx
@@ -3,7 +3,7 @@ import '../styles/reactPost.css'
 const ReactPost = (props) => {
   return (
     <div className="post-box">
-      <h3> {props.question} </h3>
+      <h3 className='question'> {props.question} </h3>
       <p> {props.answer} </p>
       <button className='likeBtn'> Like </button>
     </div>
