5ceadc1dcbaea7c53e62473e7712f4fa5daa5c74
hanifb1360 <hanif.bahari@gmail.com>
2022-09-07 01:10:40 +0200

    answers

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -2,18 +2,19 @@ export const Data =[
     {
         "id": 1,
         "question": "Vad är tre fördelar med att använda React?",
-        "answer": "low fat butter",
+        "answer": "1-Det är SEO-vänligt: En av fördelarna med React js är dess förmåga att hantera ett vanligt sökmotorfel att läsa JavaScript-tunga appar. Som en lösning kan React köras på servern, rendera och returnera den virtuella DOM till webbläsaren som en vanlig webbsida. 2- Lätt att lära och lätt att använda: En av de viktigaste fördelarna med React JS är att de flesta programmerare som har goda kunskaper i JavaScript kan bemästra React på några dagar. React är betydligt lättare att förstå än gränssnittsramverk som Angular och Vue. Det är en av de främsta anledningarna till att React har uppnått så mycket popularitet på så kort tid. 3-Det erbjuder bättre kodstabilitet: React följer nedåtgående dataflöde för att säkerställa att den överordnade strukturen inte påverkas av några ändringar i dess underordnade struktur.",
+        
         
     },
     {
         "id": 2,
         "question": "Vad betyder Single Page Application och vad skiljer det från en “vanlig” webbplats?",
-        "answer": "low fat butter",
+        "answer": "En Single Page Application är en app eller webbplats som fungerar i en webbläsare och som inte laddar om sidan medan du använder den. Allt laddas på en enda sida, som namnet antyder, istället för att ha flera sidor som de webbplatser vi alla är bekanta med (en vanlig webbsida).",
     },
     {
         "id": 3,
         "question": "Nämn tre skillnader mellan React och Angular.",
-        "answer": "low fat butter",
+        "answer": "1-Angular JS är baserat på MVC (Model View Controller) medan React är baserat på Virtual DOM. 2-Angular är baserat på Typescript och React är baserat på JavaScript. 3-AngularJS tillhandahåller inte att lägga till JavaScript-bibliotek till källkoden medan React tillåter att lägga till JavaScript-bibliotek till källkoden.",
     },
      
 ]
